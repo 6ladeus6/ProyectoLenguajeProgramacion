5c6f7641d78d87e007c48bc98c1bcec262e828fd
6ladeus6 <89492611+6ladeus6@users.noreply.github.com>
2022-05-04 21:10:28 -0500

    Capa_Datos

---
+++ CapaDatos/CDConexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class CDConexion
+    {
+        public SqlConnection conectar(string cnx)
+        {
+            try
+            {
+                SqlConnection oconectar = new SqlConnection(ConfigurationSettings.AppSettings[cnx].ToString());
+                oconectar.Open();
+                return oconectar;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
