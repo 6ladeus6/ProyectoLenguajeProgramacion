afc32a4c903ee8d8d195de9dbe7be14bd999b7cd
6ladeus6 <89492611+6ladeus6@users.noreply.github.com>
2022-05-04 21:11:02 -0500

    Capa_Entidad

---
+++ CapaEntidad/CECita.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+     public class CECita
+    {
+        private string codCita;
+        private DateTime fecha;
+        private DateTime hora;
+        private string idPaciente;
+        private string idMedico;
+        private string codCita;
+        private string codCita;
+        private string codCita;
+
+    }   
+}
